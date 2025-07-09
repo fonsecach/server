8a8d54f3be8b6035c0cba126703f39a5044e8afa
Cleiton H. da Fonseca <113487188+fonsecach@users.noreply.github.com>
2025-07-08 21:36:45 -0300

    feat: adiciona configuração do Drizzle ORM e cria tabela 'rooms' no PostgreSQL

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from 'drizzle-kit'
+import { env } from './src/env.ts'
+
+// This configuration file sets up Drizzle ORM with PostgreSQL for a TypeScript project.
+export default defineConfig({
+    dialect: 'postgresql',
+    casing: 'snake_case',
+    schema: './src/db/schema/**.ts',
+    out: './src/db/migrations',
+    dbCredentials: {
+        url: env.DATABASE_URL,
+    },
+})
